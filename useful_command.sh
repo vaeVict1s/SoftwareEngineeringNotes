9f9b4ebc96d3b51e2fd4179e829413a3726869ce
vaevictis <vaevictis@pop-os.localdomain>
2024-01-25 11:51:31 +0100

    Commit content -) useful_command.sh    regex improved    removed hardcoded file path and used a bash variable in recompilepdftex -) ai.bib - reworked to conform to standard -) Testing chapter - content added -) Containers chapter - content added to chapter and its bib file -) Smart Pointers chapter - content added to bib file

--- useful_command.sh
+++ useful_command.sh
@@ -1,11 +1,13 @@
 function cleanupconfig() {
-    find . -regextype posix-extended -regex ".*log$|.*aux$|.*out$|.*toc|.*bcf|.*xml|.*blg|.*bbl|.*blx\.bib" -exec rm "{}" \;
+    find -regextype posix-extended -regex ".*\.(log|aux|out|toc|bcf|xml|blg|bbl|-blx\.bib)$" -exec rm "{}" \;
 }
 
 function recompilepdftex() {
-    find -regextype posix-extended -regex ".*log$|.*aux$|.*out$|.*toc|.*bcf|.*xml|.*blg|.*bbl|.*blx\.bib" -exec rm "{}" \; && \  
-    pdflatex SoftWareEngineeringNotes.tex && \
-    bibtex SoftWareEngineeringNotes && \
-    pdflatex SoftWareEngineeringNotes.tex && \
-    pdflatex SoftWareEngineeringNotes.tex
+    [[ $# != 1 ]] && echo "Provide file path!" && return 1
+    filePath="$1"
+    find -regextype posix-extended -regex ".*\.(log|aux|out|toc|bcf|xml|blg|bbl|-blx\.bib)$" -exec rm "{}" \; && \  
+    pdflatex "${filePath}" && \
+    bibtex "${filePath%.tex}" && \
+    pdflatex "${filePath}" && \
+    pdflatex "${filePath}"
 }
